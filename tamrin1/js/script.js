7bc42527932a753088e39c121873f6bbba1e4020
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2023-11-25 21:35:49 +0330

    Function definition for operate

--- tamrin1/js/script.js
+++ tamrin1/js/script.js
@@ -1,7 +1,30 @@
 const display = document.querySelector(".display");
 let tempnum = " ";
-
+let firstNum,secondNum = null;
+let oldoperator;
 function getNum (x){
     tempnum = tempnum + x;
     display.innerHTML = tempnum;
+
+}
+
+function operate(type){
+    oldoperator = type;
+    if(firstNum === null && tempnum !== " "){
+        firstNum = tempnum;
+        tempnum = " ";
+    }
+    else{
+        secondNum = tempnum;
+        tempnum = " ";
+
+        switch(oldoperator){
+        case '+' :
+            
+
+
+    }
+    } 
+    
+
 }
